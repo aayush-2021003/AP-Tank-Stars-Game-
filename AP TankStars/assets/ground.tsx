c12b95265f166838019f2facdfae98080a97dc6c
aayush-2021003 <108550892+aayush-2021003@users.noreply.github.com>
2023-06-22 18:22:21 +0530

    Add files via upload

---
+++ AP TankStars/assets/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="ground" tilewidth="800" tileheight="480" tilecount="1" columns="1">
+ <image source="background.png" width="1200" height="675"/>
+</tileset>
---
+++ AP TankStars/assets/purple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="purple" tilewidth="1" tileheight="1" tilecount="480000" columns="800">
+ <image source="purple.jpg" width="800" height="600"/>
+</tileset>
